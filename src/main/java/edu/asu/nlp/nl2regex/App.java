0d55bc2edcaf3e18eba7f76e75b6b5a69275d5c3
Harshaneel Gokhale <harshaneelhg@gmail.com>
2015-11-26 11:28:29 -0700

    initial commit

---
+++ src/main/java/edu/asu/nlp/nl2regex/App.java
@@ -0,0 +1,13 @@
+package edu.asu.nlp.nl2regex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
