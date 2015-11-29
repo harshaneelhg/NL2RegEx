814ab085fc5006d4f6a5809b1074352918d8a0ba
Ashish <Ashish@Ashish-PC>
2015-11-28 17:50:34 -0700

    Changes done to handle keywords, and/or trained with more data

--- src/main/java/edu/asu/nlp/nl2regex/App.java
+++ src/main/java/edu/asu/nlp/nl2regex/App.java
@@ -9,88 +9,89 @@ import java.util.Scanner;
 import nl2kr.ccgparsing.ParseTreeNode;
 
 public class App {
-    public static void main( String[] args ) throws IOException{
-    	readUserInput();
-    	
-        Convert c = new Convert();
-        List<ParseTreeNode> functionList = c.getFuctionList();
-        System.out.println(functionList);
-        Parser p = new Parser(functionList);
-        String regEx = p.getRegEx();
-        System.out.println("RegEx: "+regEx);
-    }
-    
-    private static void readUserInput(){
-    	Scanner sc = new Scanner(System.in);
-    	
-    	System.out.println("Enter a sentence to translate:");
-    	String input = sc.nextLine();
-    	if(!input.equals("")){
-    		String cleanedInput = preprocess(input);
-    		if(!cleanedInput.equals("")){
-    			PrintWriter out;
+	public static void main(String[] args) throws IOException {
+		readUserInput();
+
+		Convert c = new Convert();
+		List<ParseTreeNode> functionList = c.getFuctionList();
+		System.out.println(functionList);
+		Parser p = new Parser(functionList);
+		String regEx = p.getRegEx();
+		System.out.println("RegEx: " + regEx);
+	}
+
+	private static void readUserInput() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a sentence to translate:");
+		String input = sc.nextLine();
+		if (!input.equals("")) {
+			String cleanedInput = preprocess(input);
+			if (!cleanedInput.equals("")) {
+				PrintWriter out;
 				try {
-					//Write to file
+					// Write to file
 					out = new PrintWriter("./Data/test.txt");
-	    			out.print(cleanedInput);
-	    			out.close();
+					out.print(cleanedInput);
+					out.close();
 				} catch (FileNotFoundException e) {
 					e.printStackTrace();
 				}
-    		}else{
-    			System.out.println("Sentence is empty!!");
-    		}
-    	}
-    }
-    
-    private static String preprocess(String input){
-    	
-    	//replace spaces inside the quotes with SP
-    	input = replaceSpaceWithSPInQuotes(input, "\"");
-    	input = replaceSpaceWithSPInQuotes(input, "'");
-    	
-    	//remove all single and double quotes
-    	input = input.replaceAll("'", "");
-    	input = input.replaceAll("\"", "");
-    	
-    	//remove all articles
-    	String [] articleList = {" the ", " a ", " an "};
-    	for(String article : articleList){
-    		input = input.replaceAll(article, "");
-    	}
-    	
-    	//remove all punctuation
-    	String [] puncList = {"\\.", "\\?", "," ,"!"};
-    	for(String punc : puncList){
-    		input = input.replaceAll(punc, "");
-    	}
-    	
-    	//remove all which with that
-    	input = input.replaceAll(" which ", " that ");
-    	
-    	//remove all in with with
-    	input = input.replaceAll(" in ", " with ");
-    	
-    	return input;
-    }
-    
-    private static String replaceSpaceWithSPInQuotes(String input, String pattern){
-    	int startIndex = 0;
-    	
-    	while(input.indexOf(pattern, startIndex) != -1 ){
-    		int firstIndex = input.indexOf(pattern, startIndex);
-    		int secondIndex = input.indexOf(pattern, firstIndex+1);
-    		//replace space
-    		String substr = input.substring(firstIndex, secondIndex);
-    		substr = substr.replace(" ", "SP");
-    		
-    		String firstpart = input.substring(0, firstIndex);
-    		String lastPart = input.substring(secondIndex+1);
-    		//recreate input with replaced spaces
-    		input = firstpart + substr + lastPart;
-    		startIndex = secondIndex+1;
-    	}
-    	
-    	return input;
-    }  
+			} else {
+				System.out.println("Sentence is empty!!");
+			}
+		}
+	}
+
+	private static String preprocess(String input) {
+
+		// replace spaces inside the quotes with SP
+		input = replaceSpaceWithSPInQuotes(input, "\"");
+		input = replaceSpaceWithSPInQuotes(input, "'");
+
+		// remove all single and double quotes
+		input = input.replaceAll("'", "");
+		input = input.replaceAll("\"", "");
+
+		// remove all articles
+		String[] articleList = { " the ", " a ", " an " };
+		for (String article : articleList) {
+			input = input.replaceAll(article, "");
+		}
+
+		// remove all punctuation
+		String[] puncList = { "\\.", "\\?", ",", "!" };
+		for (String punc : puncList) {
+			input = input.replaceAll(punc, "");
+		}
+
+		// remove all which with that
+		input = input.replaceAll(" which ", " that ");
+
+		// remove all in with with
+		input = input.replaceAll(" in ", " with ");
+
+		return input;
+	}
+
+	private static String replaceSpaceWithSPInQuotes(String input,
+			String pattern) {
+		int startIndex = 0;
+
+		while (input.indexOf(pattern, startIndex) != -1) {
+			int firstIndex = input.indexOf(pattern, startIndex);
+			int secondIndex = input.indexOf(pattern, firstIndex + 1);
+			// replace space
+			String substr = input.substring(firstIndex, secondIndex);
+			substr = substr.replace(" ", "SP");
+
+			String firstpart = input.substring(0, firstIndex);
+			String lastPart = input.substring(secondIndex + 1);
+			// recreate input with replaced spaces
+			input = firstpart + substr + lastPart;
+			startIndex = secondIndex + 1;
+		}
+
+		return input;
+	}
 }
--- src/main/java/edu/asu/nlp/nl2regex/Parser.java
+++ src/main/java/edu/asu/nlp/nl2regex/Parser.java
@@ -1,5 +1,6 @@
 package edu.asu.nlp.nl2regex;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -11,13 +12,16 @@ import nl2kr.lambda.newImpl.LVariable;
 public class Parser {
 	private List<ParseTreeNode> parseTreeList;
 	private HashMap<String, String> map;
-	public Parser(List<ParseTreeNode> functionList){
+	private HashMap<String, String> m_hmApplyParentFirst;
+	private ArrayList<Object> alIsApplied = new ArrayList<Object>();
+	private HashMap<String,String> hmKeyWordMap = new HashMap<String, String>();
+	public Parser(List<ParseTreeNode> functionList) {
 		parseTreeList = functionList;
 		map = new HashMap<>();
 		map.put("contain1", ".*:x:.*");
 		map.put("contain2", ".*:y:.*");
-		map.put("end1", ".*:x:");
-		map.put("end2", ".*:y:");
+		map.put("ends1", ".*:x:");
+		map.put("ends2", ".*:y:");
 		map.put("start1", ":x:.*");
 		map.put("start2", ":y:.*");
 		map.put("before1", ":x:.*:y:");
@@ -29,51 +33,92 @@ public class Parser {
 		map.put("letter1", ":x:");
 		map.put("letter2", ":x:");
 		map.put("not1", "~(:x:)");
-		map.put("not2", "~(:x:)");
+		map.put("not2", "~(:y:)");
 		map.put("and1", "(:x:&:y:)");
 		map.put("and2", "(:x:&:y:)");
 		map.put("or1", "(:x:|:y:)");
 		map.put("or2", "(:x:|:y:)");
 		map.put("atleast1", "(:y:){:x:}");
 		map.put("atleast2", "(:y:){:x:}");
-	}	
-	public String getRegEx(){
+
+		m_hmApplyParentFirst = new HashMap<String, String>();
+		m_hmApplyParentFirst.put("and", "contain");
+		m_hmApplyParentFirst.put("or", "contain");
+		hmKeyWordMap.put("word", "\\b[A-Z][a-z]\\b");
+		hmKeyWordMap.put("vowel", "[AEIOUaeiou]");
+		hmKeyWordMap.put("vowels", "[AEIOUaeiou]+");
+		hmKeyWordMap.put("words", "(\\b[A-Z][a-z]\\b)+");
+		hmKeyWordMap.put("number", "[0-9]");
+		hmKeyWordMap.put("numbers", "[0-9]+");
+		hmKeyWordMap.put("digits", "[0-9]+");
+	}
+
+	public String getRegEx() {
 		ParseTreeNode bestParseTree = getBestParseTreeNode();
-		if(bestParseTree != null){
+		if (bestParseTree != null) {
 			FFunction f = getFFunction(bestParseTree);
-			return parse(f);
+			return parse(f, null);
 		}
 		return "";
-	}	
-	private String parse(Object tree){
-		if(tree instanceof LVariable){
-			return ((LVariable)tree).getName();
+	}
+
+	private String parse(Object tree, Object immediateParent) {
+		if (tree instanceof LVariable) {
+			if(hmKeyWordMap.containsKey(((LVariable) tree).getName()))
+			{
+				return hmKeyWordMap.get(((LVariable) tree).getName());
+			}
+			return ((LVariable) tree).getName();
 		}
-		if(tree instanceof LNumber){
-			return ((LNumber)tree).getName();
+		if (tree instanceof LNumber) {
+			if(hmKeyWordMap.containsKey(((LNumber) tree).getName()))
+			{
+				return hmKeyWordMap.get(((LNumber) tree).getName());
+			}
+			return ((LNumber) tree).getName();
 		}
-		String fname = ((FFunction)tree).getFname().toString();
-		List arglist = ((FFunction)tree).getArguments();
+		String fname = ((FFunction) tree).getFname().toString();
+
+		List arglist = ((FFunction) tree).getArguments();
 		int len = arglist.size();
-		String ret = map.get(fname+len);
-		if(len == 2){
-			ret = ret.replace(":x:", parse(arglist.get(0)));
-			ret = ret.replace(":y:", parse(arglist.get(1)));
+		String ret = map.get(fname + len);
+		if (len == 2) {
+			String x = parse(arglist.get(0), tree);
+			String y = parse(arglist.get(1), tree);
+			String sImmediateParent = m_hmApplyParentFirst.get(fname);
+			if (sImmediateParent != null
+					&& immediateParent != null
+					&& sImmediateParent.equals(((FFunction) immediateParent)
+							.getFname().toString().trim())) {
+				x = map.get(sImmediateParent + 1).replace(":x:", x);
+				y = map.get(sImmediateParent + 1).replace(":x:", y);
+				alIsApplied.add(immediateParent);
+			}
+			if (alIsApplied.contains(tree))
+				return y;
+			ret = ret.replace(":x:", x);
+			ret = ret.replace(":y:", y);
 		}
-		if(len == 1){
-			ret = ret.replace(":x:", parse(arglist.get(0)));
+		if (len == 1) {
+			String x = parse(arglist.get(0), tree);
+			if (alIsApplied.contains(tree))
+				return x;
+			ret = ret.replace(":x:", x);
 		}
+		alIsApplied.add(tree);
 		return ret;
-	}	
-	private ParseTreeNode getBestParseTreeNode(){
-		for(ParseTreeNode tree : parseTreeList){
-			if(!tree.toString().contains("l(")){
+	}
+
+	private ParseTreeNode getBestParseTreeNode() {
+		for (ParseTreeNode tree : parseTreeList) {
+			if (!tree.toString().contains("l(")) {
 				return tree;
 			}
 		}
 		return null;
 	}
-	private FFunction getFFunction(ParseTreeNode tree){
+
+	private FFunction getFFunction(ParseTreeNode tree) {
 		return (FFunction) (tree.getNodeContent().getLambda());
 	}
 }
