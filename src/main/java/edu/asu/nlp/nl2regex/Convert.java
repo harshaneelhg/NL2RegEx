d0f0d203599a1bc14de119f86f2832c7f6deffda
ashishn1 <ashishn1@gmail.com>
2015-11-30 17:22:02 -0700

    Update Convert.java

--- src/main/java/edu/asu/nlp/nl2regex/Convert.java
+++ src/main/java/edu/asu/nlp/nl2regex/Convert.java
@@ -13,53 +13,43 @@ import nl2kr.events.TranslationEvent;
 import nl2kr.lambda.newImpl.FFunction;
 import nl2kr.scripts.NL2KR_TTest;
 
-
 class Convert {
 	private List m_functionList;
 	private boolean isTranslated;
-	Convert(){
+
+	Convert() {
 		this.isTranslated = false;
 	}
+
 	public void convert() throws IOException {
+
+		/* create data strucutres for calling parser */
 		ArrayList<String> alInputSentences = new ArrayList<String>();
 		alInputSentences.add("Data\\test.txt");
 		ArrayList<String> alSyntax = new ArrayList<String>();
 		alSyntax.add("Data\\syntax.txt");
 		ArrayList<String> aloutput = new ArrayList<String>();
 		aloutput.add("Data\\output.txt");
+		/* register event listener */
 		Bus.INSTANCE.register(this);
 		NL2KR_TTest.runTranslation(alInputSentences, aloutput, alSyntax);
-	}
-
-	public static ArrayList<String> getFileContents(InputStreamReader IOreader) throws IOException {
-		BufferedReader reader = null;
-		ArrayList<String> alFileContents = null;
-		try {
-			reader = new BufferedReader(IOreader);
-			String sContent = null;
-			alFileContents = new ArrayList<String>();
-			while ((sContent = reader.readLine()) != null) {
-				alFileContents.add(sContent);
-			}
-		} finally {
-			if (reader != null)
-				reader.close();
-		}
-		return alFileContents;
 	}
 
 	@Subscribe
 	public void handleTranslationEvent(TranslationEvent event) {
 		System.out.println("Event");
-		FFunction f = (FFunction) (event.getResults().get(0).getNodeContent().getLambda());
+		FFunction f = (FFunction) (event.getResults().get(0).getNodeContent()
+				.getLambda());
 		System.out.println(f.getFname());
 		m_functionList = event.getResults();
 		isTranslated = true;
 	}
-	public List getFuctionList() throws IOException{
+
+	public List getFuctionList() throws IOException {
 		convert();
-		while(!isTranslated);
+		while (!isTranslated)
+			;
 		return m_functionList;
 	}
-	
+
 }
